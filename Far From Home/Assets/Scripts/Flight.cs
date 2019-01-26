0a233607506d173ce15f9d45224a7475623481a2
Ryan <xryanjw@gmail.com>
2019-01-26 12:24:12 +0000

    Flight Script Updates

--- Far From Home/Assets/Scripts/Flight.cs
+++ Far From Home/Assets/Scripts/Flight.cs
@@ -4,32 +4,50 @@ using UnityEngine;
 
 public class Flight : MonoBehaviour {
 
-    Transform Player;
+    Transform player;
+    Transform playerModel;
+    Animator animator;
 
     public float forwardSpeed = 20f;
     public float horizontalSpeed = 10f;
 
+    public float turnAnimationValue;
+    public float turnAnimationTime;
+
 	void Start ()
     {
-        Player = transform.GetChild(0);
+        player = transform.GetChild(0);
+        playerModel = transform.GetChild(0);
+        animator = playerModel.GetComponent<Animator>();
 	}
 	
 	void Update ()
     {
         Move();
+
+        float rotationZ = turnAnimationTime * 25;
+        float rotationY = turnAnimationTime * -20;
+
+        //player.rotation.y = Quaternion.e;
 	}
 
     void Move ()
     {
         transform.Translate(new Vector3(0, 0, forwardSpeed * Time.deltaTime));
 
-        if (Input.GetAxisRaw("Horizontal") == 1 && Player.position.x < 7.5)
+        if (Input.GetAxisRaw("Horizontal") == 1 && player.position.x < 7.5)
         {
-            Player.Translate(new Vector2(horizontalSpeed * Time.deltaTime, 0));
+            player.Translate(new Vector2(horizontalSpeed * Time.deltaTime, 0));
+            turnAnimationValue = Mathf.Lerp(turnAnimationValue, 1, turnAnimationTime);
         }
-        if (Input.GetAxisRaw("Horizontal") == -1 && Player.position.x > -7.5)
+        else if (Input.GetAxisRaw("Horizontal") == -1 && player.position.x > -7.5)
         {
-            Player.Translate(new Vector2(-horizontalSpeed * Time.deltaTime, 0));
+            player.Translate(new Vector2(-horizontalSpeed * Time.deltaTime, 0));
+            turnAnimationValue = Mathf.Lerp(turnAnimationValue, -1, turnAnimationTime);
+        } else
+        {
+            turnAnimationValue = Mathf.Lerp(turnAnimationValue, 0.5f, turnAnimationTime);
         }
+
     }
 }
