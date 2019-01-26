66cd95b3519e105c900de7239209fecabd9900db
Ryan <xryanjw@gmail.com>
2019-01-26 10:58:30 +0000

    Basic Flight script and scene

--- Far From Home/Assets/Scripts/Flight.cs
+++ Far From Home/Assets/Scripts/Flight.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Flight : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Transform Player;
+
+    public float forwardSpeed = 20f;
+    public float horizontalSpeed = 10f;
+
+	void Start ()
+    {
+        Player = transform.GetChild(0);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Move();
 	}
+
+    void Move ()
+    {
+        transform.Translate(new Vector3(0, 0, forwardSpeed * Time.deltaTime));
+
+        if (Input.GetAxisRaw("Horizontal") == 1 && Player.position.x < 7.5)
+        {
+            Player.Translate(new Vector2(horizontalSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetAxisRaw("Horizontal") == -1 && Player.position.x > -7.5)
+        {
+            Player.Translate(new Vector2(-horizontalSpeed * Time.deltaTime, 0));
+        }
+    }
 }
